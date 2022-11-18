23ece9e0fc53e2cb4c7db0cb315fbe35fc4748f8
Arvind Varma <av894883@gmail.com>
2022-11-18 14:38:41 +0530

    change

---
+++ find_X.js
@@ -0,0 +1,6 @@
+function public_Find_x(a,b){
+    let x = Math.sqrt(3*a+1)/b;
+     console.log(` value of x = ${parseFloat(x.toFixed(2))}`)
+    }
+    public_Find_x(10.5,2.4)
+
---
+++ largestElementArray.js
@@ -0,0 +1,3 @@
+const array=[1,45,2,3,6,8,99,12];
+let result = Math.max(...array)
+ console.log(result)
