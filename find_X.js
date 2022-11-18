5a302471f25168dc2d000f1f9153e7b6f4502e9c
Arvind Varma <av894883@gmail.com>
2022-11-18 14:44:53 +0530

    same change

--- find_X.js
+++ find_X.js
@@ -4,3 +4,33 @@ function public_Find_x(a,b){
     }
     public_Find_x(10.5,2.4)
 
+//Same assignment solve java programming language
+
+// import java.util.*;
+// class Main {
+//     public static void LargestElement( int arr[]){
+//     int max = Integer.MIN_VALUE;
+//     // TRAVARS OF ELEMENT
+//       for(int i=0;i<arr.length;i++){
+//          if( arr[i]>max)
+//          max= arr[i];
+//       }
+//       // PRINT LARGEST ELEMENT
+//       System.out.print(max);
+       
+//     }
+//     public static void main(String[] args) {
+//      Scanner sc = new Scanner(System.in);
+//      int size = sc.nextInt();
+//       int arr [] = new int[size];
+//        for(int i=0;i<size;i++){
+//          arr[i] = sc.nextInt();
+//        }
+//     //   FUNCTION FIND LARGEST ELEMENT OF ARRAY
+//          LargestElement(arr);
+           
+     
+//     }
+// }
+ 
+ 
